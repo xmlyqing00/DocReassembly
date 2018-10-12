c3c3e362e9d2c369f2e9c639f9343a23ca16a0c3
yq <yq@7c-d1-c3-e8-47-59.wlan.lsu.edu>
2018-10-12 13:22:21 -0500

    replace opencv of stupid leptonica using java

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ CXX_FLAGS = $(OPT_FLAGS) --std=c++17 -Wall
 INCLUDES = -I./include/
 OPENCV_INCS = -I/usr/include/opencv2
 OPENCV_LIBS = -lopencv_shape -lopencv_stitching -lopencv_superres -lopencv_videostab -lopencv_aruco -lopencv_bgsegm -lopencv_bioinspired -lopencv_ccalib -lopencv_datasets -lopencv_dpm -lopencv_face -lopencv_fuzzy -lopencv_line_descriptor -lopencv_optflow -lopencv_video -lopencv_plot -lopencv_reg -lopencv_saliency -lopencv_stereo -lopencv_structured_light -lopencv_phase_unwrapping -lopencv_rgbd -lopencv_surface_matching -lopencv_ximgproc -lopencv_calib3d -lopencv_features2d -lopencv_flann -lopencv_xobjdetect -lopencv_objdetect -lopencv_ml -lopencv_xphoto -lopencv_highgui -lopencv_videoio -lopencv_imgcodecs -lopencv_photo -lopencv_imgproc -lopencv_core
-TESSARACT_LIBS = -llept -ltesseract
+TESSARACT_LIBS = -ltesseract
 
 src_dir = ./src/
 dst_dir = ./
@@ -17,12 +17,12 @@ default: StripeReassembly
 .PHONY: clean
 
 %.o: %.cpp
-	$(CXX) -c $< -o $@ -MMD $(CXX_FLAGS) $(INCLUDES) $(OPENCV_INCS) 
+	$(CXX) -c $< -o $@ -MMD $(CXX_FLAGS) $(INCLUDES) 
 
 -include $(dep)
 
 StripeReassembly: $(obj)
-	$(CXX) $< $(TESSARACT_LIBS) $(OPENCV_LIBS) -o $(dst_dir)$@
+	$(CXX) $^ $(TESSARACT_LIBS) $(OPENCV_LIBS) -o $(dst_dir)$@
 
 clean:
 	rm $(dst)StripeReassembly $(obj) $(dep)
--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,63 @@
+#include <string>
 #include <tesseract/baseapi.h>
-#include <leptonica/allheaders.h>
+#include <opencv2/opencv.hpp>
 
-int main()
-{
+int main(int argc, char ** argv) {
+
+    std::string img_path = argv[1];
+    std::cout << argc << std::endl;
+    std::cout << img_path << std::endl;
 
     tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+    
+    printf("at\n");
     // Initialize tesseract-ocr with English, without specifying tessdata path
-    if (api->Init(NULL, "eng+chi_sim")) {
+    if (api->Init(NULL, "eng")) {
         fprintf(stderr, "Could not initialize tesseract.\n");
-        exit(1);
+        return 1;
     }
 
-    // Open input image with leptonica library
-    Pix *image = pixRead("./data/test0.png");
-    api->SetImage(image); 
+    cv::Mat img = cv::imread(img_path);
+    api->SetImage(img.data, img.cols, img.rows, 3, img.step);
 
-    // Get OCR result
+    std::string out_text(api->GetUTF8Text());
+
+    std::cout<< out_text << std::endl;
+
+    api->End();
+
+    // printf("start\n");
+
+    // // Open input image with leptonica library
+    // Pix *image = pixRead("./data/test0.png");
+    // api->SetImage(image); 
+
+    // // // Get OCR result
     // char * outText = api->GetUTF8Text();
     // int conf = api->MeanTextConf();
     // printf("OCR output:\n%s", outText);
     // printf("Confidence: %d\n", conf);
     // delete [] outText;
 
-    api->Recognize(0);
-    int b;
-    tesseract::ResultIterator* ri = api->GetIterator();
-    tesseract::PageIteratorLevel level = tesseract::RIL_SYMBOL;
-    if (ri != 0) {
-        do {
-        const char* word = ri->GetUTF8Text(level);
-        float conf = ri->Confidence(level);
-        int x1, y1, x2, y2;
-        ri->BoundingBox(level, &x1, &y1, &x2, &y2);
-        printf("word: '%s';  \tconf: %.2f; BoundingBox: %d,%d,%d,%d;\n",
-                   word, conf, x1, y1, x2, y2);
-        delete[] word;
-        } while (ri->Next(level));
-    }
+    // // // api->Recognize(0);
+    // // // int b;
+    // // // tesseract::ResultIterator* ri = api->GetIterator();
+    // // // tesseract::PageIteratorLevel level = tesseract::RIL_WORD;
+    // // // if (ri != 0) {
+    // // //     do {
+    // // //     const char* word = ri->GetUTF8Text(level);
+    // // //     float conf = ri->Confidence(level);
+    // // //     int x1, y1, x2, y2;
+    // // //     ri->BoundingBox(level, &x1, &y1, &x2, &y2);
+    // // //     printf("word: '%s';  \tconf: %.2f; BoundingBox: %d,%d,%d,%d;\n",
+    // // //                word, conf, x1, y1, x2, y2);
+    // // //     delete[] word;
+    // // //     } while (ri->Next(level));
+    // // // }
 
-    // Destroy used object and release memory
-    api->End();
-    pixDestroy(&image);
+    // // Destroy used object and release memory
+    // api->End();
+    // pixDestroy(&image);
 
     return 0;
 }
