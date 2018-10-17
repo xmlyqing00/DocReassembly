1ed6141233c173de7f0b6cf42bb960b7db610461
yq <yqliang@cct.lsu.edu>
2018-10-17 13:44:18 -0500

    tmp

--- include/stripes.h
+++ include/stripes.h
@@ -20,12 +20,18 @@ public:
 
     Stripes();
     void push(const cv::Mat & stripe_img);
+    
     bool reassemble(Composition comp_mode);
+
+    double m_metric_word(const cv::Mat & frag0, const cv::Mat & frag1);
 
 private:
 
+    void merge_frags(const cv::Mat & in_frag0, const cv::Mat & in_frag1, cv::Mat & out_frag);
+
     bool reassemble_greedy();
-    
+
+
 };
 
 #endif
--- src/stripes.cpp
+++ src/stripes.cpp
@@ -21,12 +21,58 @@ bool Stripes::reassemble(Composition comp_mode) {
 
 }
 
+double Stripes::m_metric_word(const cv::Mat & frag0, const cv::Mat & frag1) {
+
+}
+
+void Stripes::merge_frags(const cv::Mat & in_frag0, const cv::Mat & in_frag1, cv::Mat & out_frag) {
+
+    assert(in_frag0.rows == in_frag1.rows);
+
+    cv::Size out_frag_size(in_frag0.cols + in_frag1.cols, in_frag0.rows);
+    out_frag = cv::Mat(out_frag_size, CV_8UC3);
+    in_frag0.copyTo(out_frag);
+}
+
 bool Stripes::reassemble_greedy() {
 
+    double max_avg_m_score = 0;
+
     for (int i = 0; i < stripes_n; i++) {
-        for (int j = 0; j < stripes_n; j++) {
-            if (i == j) continue;
+
+        vector<bool> inclusion_flag(stripes_n, false);
+        cv::Mat frag = stripes[i].clone();
+        inclusion_flag[i] = true;
+        double avg_m_score = 0;
+        
+        for (int inclusion_cnt = 1; inclusion_cnt < stripes_n; inclusion_cnt++) {
+            
+            double max_m_score = 0;
+            int max_m_score_idx = 0;
+
+            for (int j = 0; j < stripes_n; j++) {
+
+                if (inclusion_flag[j]) continue;
+
+                double m_score = m_metric_word(frag, stripes[i]);
+                if (max_m_score < m_score) {
+                    max_m_score = m_score;
+                    max_m_score_idx = j;
+                }
+
+            }
+
+            inclusion_flag[max_m_score_idx] = true;
+            avg_m_score += max_m_score;
+            merge_frags(frag, stripes[max_m_score_idx], frag);
+
         }
+
+        avg_m_score /= stripes_n - 1;
+        if (max_avg_m_score < avg_m_score) {
+            max_avg_m_score = avg_m_score;
+        }
+
     }
 
     return true;
