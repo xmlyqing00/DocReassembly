a96396a50aa30b38abc5a2c918f86d43679c6f24
LyqSpace <root@lyq.me>
2018-11-11 23:06:25 -0600

    add symbols

---
+++ data/symbols/auto_text2image.sh
@@ -0,0 +1,16 @@
+text2image \
+	--text symbols.txt \
+	--outputbase symbol \
+	--font='Arial' \
+    --fonts_dir /usr/share/fonts/ \
+	--rotate_image=false \
+	--box_padding 4 \
+	--glyph_resized_size 32 \
+	--output_individual_glyph_images \
+	
+	
+	
+    
+
+
+
