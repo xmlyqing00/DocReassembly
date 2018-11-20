502be499c149ab6103a7b4d8abb4345812d29fce
LyqSpace <root@lyq.me>
2018-11-20 11:21:40 -0600

    update symbols

--- data/symbols/auto_text2image.sh
+++ data/symbols/auto_text2image.sh
@@ -1,16 +1,9 @@
 text2image \
 	--text symbols.txt \
-	--outputbase symbol \
-	--font='Arial' \
+	--outputbase arial_bold_italic \
+	--font='Arial Bold Italic' \
     --fonts_dir /usr/share/fonts/ \
 	--rotate_image=false \
 	--box_padding 4 \
 	--glyph_resized_size 32 \
 	--output_individual_glyph_images \
-	
-	
-	
-    
-
-
-
