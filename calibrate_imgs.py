4bcc5303a9ad08926df6f0fde30485b1c2cd9415
yq <yqliang@cct.lsu.edu>
2019-01-10 13:47:39 -0600

    tmp

--- calibrate_imgs.py
+++ calibrate_imgs.py
@@ -1,6 +1,10 @@
 import numpy as np
 import cv2
 
+def calc_areas(contour):
+    pass
+    return 1
+    
 def calibrate_imgs():
 
     img = cv2.imread('data/real_test/real0_raw_21/19.jpg')
@@ -9,7 +13,16 @@ def calibrate_imgs():
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img_canny = cv2.Canny(img_gray, 200, 300)
     img2, contours, hierarchy = cv2.findContours(img_canny, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(img, contours, -1, (0, 255, 0))
+
+    max_area = 0
+    max_area_id = 0
+    for i in range(len(contours)):
+        area = calc_areas(contours[i])
+        if max_area < area:
+            max_area = area
+            max_area_id = i
+
+    # cv2.drawContours(img, contours, -1, (0, 255, 0))
     print(len(contours))
     # print(b.shape)
     cv2.imshow('img', img)
