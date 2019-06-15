95affff51393b4248a90311a1e96ffaa0a6dc5fc
yq <yqliang@cct.lsu.edu>
2019-06-15 22:43:43 +0800

    tmp

--- benchmark.sh
+++ benchmark.sh
@@ -28,6 +28,36 @@ if [ "$1" == "GA" ]; then
     exit
 fi
 
+if [ "$1" == "doc" ]; then
+    echo "Run doc dataset."
+
+    test_files=("doc0" "doc3" "doc7" "doc10" "doc11" "doc12" "doc13" "doc14" "doc15" "doc16" "doc17" "doc18" "doc19" "doc20" "doc21" "doc23" "doc24")
+    for test_name in ${test_files[@]}; do
+
+        echo "Test case:" ${test_name} "==========="
+        for i in $(seq 0 ${test_n}); do
+            ./bin/release/generator -t ${test_name} -n ${nums[i]}
+            echo " "
+        done
+
+        for i in $(seq 0 ${method_n}); do
+            for j in $(seq 0 ${test_n}); do
+                echo "Test args:" ${i} ${j}
+                ./bin/release/solver \
+                    -t ${test_name} \
+                    -n ${nums[j]} \
+                    -m ${metrics[i]} \
+                    -c ${comps[i]} \
+                    -s ${samples[j]} \
+                    -b
+                echo "  "
+            done
+        done
+    done
+
+    exit
+fi
+
 echo "Test case: "$1
 for i in $(seq 0 ${test_n}); do
     ./bin/release/generator -t $1 -n ${nums[i]}
